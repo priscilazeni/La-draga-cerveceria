fca0e7f6d9cb707d7b48a3824bac96c125804311
Priscila Zeniquel <priscilazeniquel@hotmail.com>
2023-05-07 13:12:38 -0300

    Actualizacion de las fotos y estilos

--- JS/data.js
+++ JS/data.js
@@ -171,7 +171,212 @@ let productos = [
         "img" : "./multimedia/nuggets.jpeg",
         "ingredientes": "nuggets de pollo x 20 + dip ketchup + dip salsa ranch"
 
+    },
+
+    {
+
+        "id": "16",
+        "nombre": "pepsi regular 354ml",  
+        "cantidad": "1", 
+        "precio": 450, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/pepsi.png",
+        "ingredientes": "pepsi regular 354ml"
+
+    },
+
+    {
+
+        "id": "17",
+        "nombre": "pepsi black 354ml",  
+        "cantidad": "1", 
+        "precio": 450, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/pepsi-black.png",
+        "ingredientes": "pepsi black 354ml"
+
+    },
+
+    {
+
+        "id": "18",
+        "nombre": "seven up regular 354ml",  
+        "cantidad": "1", 
+        "precio": 450, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/seven-up.png",
+        "ingredientes": "seven up regular 354ml"
+
+    },
+
+    {
+
+        "id": "19",
+        "nombre": "seven up free 354ml",  
+        "cantidad": "1", 
+        "precio": 450, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/seven-up-free.png",
+        "ingredientes": "seven up free 354ml"
+
+    },
+
+    {
+
+        "id": "20",
+        "nombre": "agua mineral sin gas eco de los andes",  
+        "cantidad": "1", 
+        "precio": 500, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/agua.png",
+        "ingredientes": "agua mineral sin gas eco de los andes"
+
+    },
+
+    {
+
+        "id": "21",
+        "nombre": "cerveza patagonia",  
+        "cantidad": "1", 
+        "precio": 650, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/patagonia.png",
+        "ingredientes": "cerveza patagonia 24.7 410ml"
+
+    },
+
+    {
+
+        "id": "22",
+        "nombre": "cerveza patagonia bohemian 410ml",  
+        "cantidad": "1", 
+        "precio": 650, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/patagonia-bohemian.png",
+        "ingredientes": "cerveza patagonia bohemian 410ml"
+
+    },
+
+    {
+
+        "id": "23",
+        "nombre": "cerveza patagonia amber lager 410ml",  
+        "cantidad": "1", 
+        "precio": 650, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/patagonia-amber.png",
+        "ingredientes": "cerveza patagonia amber lager 410ml"
+
+    },
+
+    {
+
+        "id": "24",
+        "nombre": "cerveza patagonia sol citra 410ml",  
+        "cantidad": "1", 
+        "precio": 650, 
+        "categoria": "bebidas", 
+        "img" : "./multimedia/patagonia-sol-citra.png",
+        "ingredientes": "cerveza patagonia sol citra 410ml"
+
+    },
+
+    {
+
+        "id": "25",
+        "nombre": "bbq",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/bbq.jpeg",
+        "ingredientes": "dip salsa bbq"
+
+    },
+
+    {
+
+        "id": "26",
+        "nombre": "salsa drag",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/drag.jpeg",
+        "ingredientes": "dip salsa drag"
+
+    },
+
+    {
+
+        "id": "27",
+        "nombre": "california sauce",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/california.jpeg",
+        "ingredientes": "dip california sauce"
+
+    },
+
+    {
+
+        "id": "28",
+        "nombre": "marinara",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/marinara.jpeg",
+        "ingredientes": "dip salsa marinara"
+
+    },
+
+    {
+
+        "id": "29",
+        "nombre": "relish sauce",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/relish.jpeg",
+        "ingredientes": "dip relish sauce"
+
+    },
+
+    {
+
+        "id": "30",
+        "nombre": "ali oli",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/ali-oli.jpeg",
+        "ingredientes": "dip salsa ali oli"
+
+    },
+
+    {
+
+        "id": "31",
+        "nombre": "ali oli ahumado",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/ali-oli-ahumado.jpeg",
+        "ingredientes": "dip salsa ali oli ahumado"
+
+    },
+
+    {
+
+        "id": "32",
+        "nombre": "ali oli jalapeños",  
+        "cantidad": "1", 
+        "precio": 200, 
+        "categoria": "salsas", 
+        "img" : "./multimedia/ali-oli-jalapeno.jpeg",
+        "ingredientes": "dip salsa ali oli jalapeño"
+
     }
+
 
 ]
 
--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,132 @@
 const menuHamburguesas = document.getElementById("menuHamburguesas");
+
 const menuStarters = document.getElementById("menuStarters");
 
+const menuBebidas = document.getElementById("menuBebidas");
 
-// fetch('./JSON/data.json')
-// .then((response) => response.json())
-// .then((data) => {
-//     productos = data;
-//     mostrarMenu(productos);
-// })
+const menuSalsas = document.getElementById("menuSalsas");
+
+let hamburguesas = productos.filter((prod) => prod.categoria === "hamburguesas")
+
+let starters = productos.filter((prod) => prod.categoria === "starters")
+
+let bebidas = productos.filter((prod) => prod.categoria === "bebidas")
+
+let salsas = productos.filter((prod) => prod.categoria === "salsas")
 
 
 // Renderizacion del menu //
 
-function mostrarMenu (array) {
+// HAMBURGUESAS
+
+function mostrarHamburguesas (array) {
 
         menuHamburguesas.innerHTML= '';
         array.forEach((producto) => {
         const divProd = document.createElement("div");
         divProd.classList.add("contenedor");
         divProd.innerHTML = `
-            <p class="nombre">${producto.nombre.toUpperCase()}</p>
-            <p class="ingredientes">${producto.ingredientes}</p>
-            <p class="precio">$${producto.precio}</p>
-            <img src="${producto.img}" alt="${producto.nombre}" class="imgProducto">
-
-        <input type="button" value="AGREGAR" id="${producto.id}" class="btnAgregar">`
+        <div class="contenedorDos">
+            <div class="contenedorImg">    
+                <img src="${producto.img}" alt="${producto.nombre}" class="imgProductoHamburguesas">
+            </div> 
+            <div class="contenedorTres">
+                <p class="nombre">${producto.nombre.toUpperCase()}</p>
+                <p class="ingredientes">${producto.ingredientes}</p>
+                <p class="precio">$${producto.precio}</p>
+            </div>
+        </div>    
+            <input type="button" value="AGREGAR" id="${producto.id}" class="btnAgregar">`
 
         menuHamburguesas.appendChild(divProd);
     });
 
 };
 
-mostrarMenu(productos)
+mostrarHamburguesas(hamburguesas);
+
+// STARTERS
+
+
+function mostrarStarters (array) {
+
+    menuStarters.innerHTML= '';
+    array.forEach((producto) => {
+    const divProd = document.createElement("div");
+    divProd.classList.add("contenedor");
+    divProd.innerHTML = `
+    <div class="contenedorDos">
+        <div class="contenedorImg">    
+            <img src="${producto.img}" alt="${producto.nombre}" class="imgProductoStarters">
+        </div> 
+        <div class="contenedorTres">
+            <p class="nombre">${producto.nombre.toUpperCase()}</p>
+            <p class="ingredientes">${producto.ingredientes}</p>
+            <p class="precio">$${producto.precio}</p>
+        </div>
+    </div>    
+        <input type="button" value="AGREGAR" id="${producto.id}" class="btnAgregar">`
+
+    menuStarters.appendChild(divProd);
+});
+
+};
+
+mostrarStarters(starters);
+
+// BEBIDAS
+
+
+function mostrarBebidas (array) {
+
+    menuBebidas.innerHTML= '';
+    array.forEach((producto) => {
+    const divProd = document.createElement("div");
+    divProd.classList.add("contenedor");
+    divProd.innerHTML = `
+    <div class="contenedorDos">
+        <div class="contenedorImg">    
+            <img src="${producto.img}" alt="${producto.nombre}" class="imgProductoBebidas">
+        </div> 
+        <div class="contenedorTres">
+            <p class="nombre">${producto.nombre.toUpperCase()}</p>
+            <p class="ingredientes">${producto.ingredientes}</p>
+            <p class="precio">$${producto.precio}</p>
+        </div>
+    </div>    
+        <input type="button" value="AGREGAR" id="${producto.id}" class="btnAgregarBebidas">`
+
+    menuBebidas.appendChild(divProd);
+});
+
+};
+
+mostrarBebidas(bebidas);
+
+// SALSAS
+
+function mostrarSalsas (array) {
+
+    menuSalsas.innerHTML= '';
+    array.forEach((producto) => {
+    const divProd = document.createElement("div");
+    divProd.classList.add("contenedor");
+    divProd.innerHTML = `
+    <div class="contenedorDos">
+        <div class="contenedorImg">    
+            <img src="${producto.img}" alt="${producto.nombre}" class="imgProductoSalsas">
+        </div> 
+        <div class="contenedorTres">
+            <p class="nombre">${producto.nombre.toUpperCase()}</p>
+            <p class="ingredientes">${producto.ingredientes}</p>
+            <p class="precio">$${producto.precio}</p>
+        </div>
+    </div>    
+        <input type="button" value="AGREGAR" id="${producto.id}" class="btnAgregar">`
+
+    menuSalsas.appendChild(divProd);
+});
+
+};
+
+mostrarSalsas(salsas);
